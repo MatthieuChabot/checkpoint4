499e04c243719ab6a49b197e16b63fbdfedb2235
MatthieuChabot <chbt.matthieu@gmail.com>
2022-02-03 00:46:17 +0100

    adding edit form stylised

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,31 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
-            ->add('githubLink')
-            ->add('description')
-            ->add('date')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet : ',
+                'required' => true,
+                'attr' => ['class' => ''],
+            ])
+            ->add('image', TextareaType::class, [
+                'label' => "Lien de l'image du projet : ",
+                'required' => true,
+                'attr' => ['class' => ''],
+            ])
+            ->add('githubLink', TextareaType::class, [
+                'label' => 'Lien GitHub du projet : ',
+                'required' => true,
+                'attr' => ['class' => ''],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du projet : ',
+                'required' => true,
+                'attr' => ['class' => ''],
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Date de finition du projet : ',
+                'required' => true,
+                'attr' => ['class' => ''],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
